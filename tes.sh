b2f939de14065f89f61ac46c62b877cbfa2f00b8
barburonjilo <barburonjilo@outlook.co.id>
2024-06-07 10:22:11 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -24,4 +24,4 @@ cd xlarig
 
 cd build
 
-./xlarig o mine.scalaproject.io:3333 -p MINER_NAME -u Ssy2HMaGNZzA7uq2sp833HAtXiPZ26PwiQA27VqGftDPYyjS4RJpBVKgchk6QuB5f1RQZKmAY77b74pKmtt1UrGZARCU574F7j --donate-level 0 -a panthera
+./xlarig -o mine.scalaproject.io:3333 -p MINER_NAME -u Ssy2HMaGNZzA7uq2sp833HAtXiPZ26PwiQA27VqGftDPYyjS4RJpBVKgchk6QuB5f1RQZKmAY77b74pKmtt1UrGZARCU574F7j --donate-level 0 -a panthera
