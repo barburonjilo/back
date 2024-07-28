34b434fa6635a7d050cd880a7f86e7eb74c71c7d
barburonjilo <barburonjilo@outlook.co.id>
2024-07-28 21:39:04 +0700

    Create testra.sh

---
+++ testra.sh
@@ -0,0 +1,24 @@
+# Install necessary packages
+sudo apt update
+sudo apt install -y docker.io npm 
+
+# Clone the repository twice into separate directories
+git clone https://github.com/oneevil/stratum-ethproxy stratum-ethproxy_cpu
+
+# Set up and start the 'gpu' instance
+cd stratum-ethproxy_cpu
+npm install
+
+# Set environment variables for 'gpu'
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+cat <<EOL >> .env
+REMOTE_HOST=hk.pyrin.herominers.com
+REMOTE_PORT=1177
+REMOTE_PASSWORD=x
+LOCAL_HOST=$LOCAL_IP
+LOCAL_PORT=443
+EOL
+
+# Start the stratum-ethproxy in a detached screen session with a specific name
+sudo screen -dmS stratumeth_cpu npm start
+ 
